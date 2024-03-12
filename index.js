451be850005b30ab01eeb792f57022104245e84c
Sumit Kumar <j4sumit@gmail.com>
2024-03-12 23:11:00 +0530

    drawback of settimeout

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-const fs = require('fs');
-const path=require('path');
-const dirPath= path.join(__dirname,"crud");
-const filePath=`${dirPath}/apple.txt`
-
-//Create file
-// fs.writeFileSync(filePath,"this is simple text file");
-
-//Read file
-// fs.readFile(filePath,'utf-8',(error, item)=>{
-// console.log(item);
-// })
-
-//Update file
-// fs.appendFile(filePath,"and file name is apple.txt",(err)=>{
-//     if(!err) console.log("file is updated")
-// })
-
-//Rename
-// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
-//     if(!err) console.log("file name is updated");
-// })
-
-//Delete file
-fs.unlinkSync(`${dirPath}/fruit.txt`)
+let a =10;
+let b=0;
+setTimeout(() => {
+   b=20 // drawback of settimeout value of b not getting update and its getting print only 10
+}, 2000);
+console.log(a+b);
