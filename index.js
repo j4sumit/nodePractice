074a5f94d488458ada0b7ebf660ad40d9270f3e8
Sumit Kumar <j4sumit@gmail.com>
2024-03-12 19:00:49 +0530

    Simple API creation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const data= require('./data')
 const http = require('http')
 http.createServer((req,res)=>{
-res.writeHead(200,{'content-type': "application\json"})
-res.write(JSON.stringify({
-    name: "Sumit",
-    email:"sumit@gmail.com"
-}));
+res.writeHead(205,{'content-type': "application\json"})
+// res.write(JSON.stringify({
+//     name: "Sumit",
+//     email:"sumit@gmail.com"
+// }));
+res.write(JSON.stringify(data));
 res.end()
 }).listen(5000)
