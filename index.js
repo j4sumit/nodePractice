3053979461a45907a39f747ef3e6449d84fe650a
Sumit Kumar <j4sumit@gmail.com>
2024-03-12 22:06:27 +0530

    getting input from command line

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const data= require('./data')
-const http = require('http')
-http.createServer((req,res)=>{
-res.writeHead(205,{'content-type': "application\json"})
-// res.write(JSON.stringify({
-//     name: "Sumit",
-//     email:"sumit@gmail.com"
-// }));
-res.write(JSON.stringify(data));
-res.end()
-}).listen(5000)
+// console.log(process.argv) /*[
+//     'C:\\Program Files\\nodejs\\node.exe',
+//     'C:\\Users\\j4sum\\OneDrive\\Desktop\\nodePractice\\index.js',
+//     'hello',
+//     'hi'
+//   ]*/
+// console.log(process.argv[0]) //C:\Program Files\nodejs\node.exe
+// console.log(process.argv[1]) //C:\Users\j4sum\OneDrive\Desktop\nodePractice\index.js
+// console.log("node index.js hello hi") //node index.js hello hi
+
+const fs = require('fs')
+const input = process.argv;
+if(input[2]=='add')
+{
+    fs.writeFileSync(input[3],input[4])
+}else if(input[2]=='remove'){
+    fs.unlinkSync(input[3])
+}else{
+    console.log("invalid content");
+}
