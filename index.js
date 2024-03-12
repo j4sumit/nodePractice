872f4c2d6bac54f6ebf3f004bd758e7534564c5c
Sumit Kumar <j4sumit@gmail.com>
2024-03-12 23:24:25 +0530

    Asynchronous Operation

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let a =10;
+// let a =10;
+// let b=0;
+// setTimeout(() => {
+//    b=20 // drawback of asynchronous, value of b not getting update and its getting print only 10
+// }, 2000);
+// console.log(a+b);
+
+//Promise Example
+let a=20;
 let b=0;
+let waitingData= new Promise((resolve, reject)=>{
 setTimeout(() => {
-   b=20 // drawback of settimeout value of b not getting update and its getting print only 10
+    resolve(50)
 }, 2000);
-console.log(a+b);
+})
+
+waitingData.then((data)=>{
+    b=data;
+    console.log(a+b);
+})
