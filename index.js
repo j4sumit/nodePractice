bcd232b05ae001b853bfb4a93093ddcd985ef7c2
Sumit Kumar <j4sumit@gmail.com>
2024-03-12 22:25:07 +0530

    Display file list from folder

--- index.js
+++ index.js
@@ -8,13 +8,27 @@
 // console.log(process.argv[1]) //C:\Users\j4sum\OneDrive\Desktop\nodePractice\index.js
 // console.log("node index.js hello hi") //node index.js hello hi
 
-const fs = require('fs')
-const input = process.argv;
-if(input[2]=='add')
-{
-    fs.writeFileSync(input[3],input[4])
-}else if(input[2]=='remove'){
-    fs.unlinkSync(input[3])
-}else{
-    console.log("invalid content");
-}
+// const fs = require('fs')
+// const input = process.argv;
+// if(input[2]=='add')
+// {
+//     fs.writeFileSync(input[3],input[4])
+// }else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3])
+// }else{
+//     console.log("invalid content");
+// }
+
+const fs = require('fs');
+const path= require('path');
+const dirPath = path.join(__dirname,"files");
+// for(i=0; i<8; i++){
+//     // fs.writeFileSync(dirPath+"/hello"+i+".txt","A simple text file");
+//     fs.writeFileSync(`${dirPath}/hello${i}.txt`,"A simple text file");
+// }
+
+fs.readdir(dirPath,(err,files)=>{
+files.forEach((item)=>{
+    console.log("files name is",item)
+})
+})
