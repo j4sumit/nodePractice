0c10153849b2453554c7f53cd1a6a48f2a6f010c
Sumit Kumar <j4sumit@gmail.com>
2024-03-12 22:56:05 +0530

    Crud Operation with file System

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-// console.log(process.argv) /*[
-//     'C:\\Program Files\\nodejs\\node.exe',
-//     'C:\\Users\\j4sum\\OneDrive\\Desktop\\nodePractice\\index.js',
-//     'hello',
-//     'hi'
-//   ]*/
-// console.log(process.argv[0]) //C:\Program Files\nodejs\node.exe
-// console.log(process.argv[1]) //C:\Users\j4sum\OneDrive\Desktop\nodePractice\index.js
-// console.log("node index.js hello hi") //node index.js hello hi
-
-// const fs = require('fs')
-// const input = process.argv;
-// if(input[2]=='add')
-// {
-//     fs.writeFileSync(input[3],input[4])
-// }else if(input[2]=='remove'){
-//     fs.unlinkSync(input[3])
-// }else{
-//     console.log("invalid content");
-// }
-
 const fs = require('fs');
-const path= require('path');
-const dirPath = path.join(__dirname,"files");
-// for(i=0; i<8; i++){
-//     // fs.writeFileSync(dirPath+"/hello"+i+".txt","A simple text file");
-//     fs.writeFileSync(`${dirPath}/hello${i}.txt`,"A simple text file");
-// }
+const path=require('path');
+const dirPath= path.join(__dirname,"crud");
+const filePath=`${dirPath}/apple.txt`
 
-fs.readdir(dirPath,(err,files)=>{
-files.forEach((item)=>{
-    console.log("files name is",item)
-})
-})
+//Create file
+// fs.writeFileSync(filePath,"this is simple text file");
+
+//Read file
+// fs.readFile(filePath,'utf-8',(error, item)=>{
+// console.log(item);
+// })
+
+//Update file
+// fs.appendFile(filePath,"and file name is apple.txt",(err)=>{
+//     if(!err) console.log("file is updated")
+// })
+
+//Rename
+// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
+//     if(!err) console.log("file name is updated");
+// })
+
+//Delete file
+fs.unlinkSync(`${dirPath}/fruit.txt`)
