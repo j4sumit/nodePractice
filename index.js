4daf6828d23c87332eaa9cf44bef7e004fbb5f17
Sumit Kumar <j4sumit@gmail.com>
2024-03-13 12:34:03 +0530

    Render HTML and JSON

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app =express()
-app.get("",(req,res)=>{
-res.send("hello, this is home page")
+app.get("",(req,res)=>{  
+res.send(`<h1>Welcome to Home page</h1>
+<a href="/about">Go to about Page</a>
+`)
 })
 
 app.get("/about",(req,res)=>{
-res.send("This is about page")
+res.send(`
+<input type="text" placeholder="Enter some data here" value='${req.query.name}'/><br />
+<button>Submit</button>
+<a href="/">Go to Home Page</a>
+`)
 })
 
 app.get("/contact",(req,res)=>{
-    res.send("This is contact page")
+    res.send([{
+        name:"Sumit",
+        email:"Sumit@gmail.com"
+    },
+    {
+        name:"Amit",
+        email:"Amit@gmail.com"
+    }
+])
     })
 
 app.listen(4000)
