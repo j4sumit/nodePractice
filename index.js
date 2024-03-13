646c593113a8392ae43a7645f6c39525f60a5c6f
Sumit Kumar <j4sumit@gmail.com>
2024-03-13 15:26:07 +0530

    EJS Template Engine/Create dynamic page

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const path= require('path'); //to access foloder location
-console.log("dirname",__dirname); //C:\Users\j4sum\OneDrive\Desktop\nodePractice
 
 const app =express()
 const publicPath=path.join(__dirname,"public")
-console.log("publicPath",publicPath); //C:\Users\j4sum\OneDrive\Desktop\nodePractice\public
-app.use(express.static(publicPath)) //Static method load static page
+
+app.set('view engine', "ejs");   //1
+app.get("/profile",(req,res)=>{  //2
+    const user={
+        name:"Sumit",
+        email:"sumit@gmail.com",
+        city:"Mumbai"
+    }
+    res.render('profile', {user})
+})
+
+
+app.get("",(req,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+app.get("/aboutme",(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+app.get("/help",(req,res)=>{
+    res.sendFile(`${publicPath}/help.html`)
+})
+app.get("*",(req,res)=>{
+    res.sendFile(`${publicPath}/pageNotfound.html`)
+})
 
 app.listen(4000)
