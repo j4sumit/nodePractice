b629209c0af4dcdb143e4d3ee938fe5c9c3d3508
Sumit Kumar <j4sumit@gmail.com>
2024-03-15 11:27:09 +0530

    Mongoose with node connection

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const dbConnect= require('./mongodb')
-//console.log(dbConnect());// Its returning Promise
-//Using Promise
-dbConnect().then((resp)=>{
-resp.find().toArray().then((data)=>{
-    console.log(data)
-})
-})
+const mongoose = require('mongoose');
 
-//Using Async await
-// const main =async ()=>{
-// let data= await dbConnect();
-// data=await data.find().toArray();
-// console.log(data)
-// }
-// main()
+const main=async()=>{
+await mongoose.connect('mongodb://127.0.0.1:27017/SumitDatabase')  //Database name-SumitDatabase
+
+const ProductSchema= new mongoose.Schema({
+    name:String,
+    age:Number
+});
+const ProductsModel = mongoose.model('product',ProductSchema) //Collection name-product
+let data = new ProductsModel({name:"Radha",age:5});
+let result =await data.save()
+console.log(result)
+}
+
+main();
