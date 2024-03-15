ce3d0f5bfae597d5d1232b166b55e84f9c5a3941
Sumit Kumar <j4sumit@gmail.com>
2024-03-15 16:53:43 +0530

    Search API with multiple filed

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express =require('express')
 require('./config')
-const products=require('./product')
+const product=require('./product')
 const app=express()
 app.use(express.json())// to get req.body data
 
-app.post("/create",async(req,res)=>{
-    let data= new products(req.body);
-    let result= await data.save()
-    console.log(result)
-res.send(result)
+app.get("/search/:key",async (req,res)=>{
+     let key = req.params.key;
+    let ageQuery = {};
+
+    if (!isNaN(key)) {
+        ageQuery = { "age": parseInt(key) };// here we are checking if input is number then check in age column
+    }
+    console.log("key",key)
+    console.log("ageQuery",ageQuery)
+
+    let data = await product.find(
+        {
+            "$or":[
+                {"name":{$regex:req.params.key,$options: 'i'}},
+                {"state":{$regex:req.params.key, $options: 'i'}},
+                ageQuery
+            ]
+        }
+    )
+res.send(data);
 })
+
 app.listen(5000)
-
-
-app.get("/list",async(req,res)=>{
-  let data=await products.find();
-  res.send(data);
-})
-
-app.delete("/delete/:_id",async(req,res)=>{
-console.log(req.params)
- let data = await products.deleteOne(req.params)
-res.send(data)
-})
-
-app.put("/update/:_id",async(req,res)=>{
-     let data = await products.updateOne(
-        req.params,
-        {$set:req.body}
-     )
-    res.send(data)
-    })
