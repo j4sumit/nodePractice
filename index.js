eda35918f4b0709c42c708ec492986fe884da731
Sumit Kumar <j4sumit@gmail.com>
2024-03-15 15:02:10 +0530

    GET, DELETE and PUT api method with mongoose

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ app.post("/create",async(req,res)=>{
 res.send(result)
 })
 app.listen(5000)
+
+
+app.get("/list",async(req,res)=>{
+  let data=await products.find();
+  res.send(data);
+})
+
+app.delete("/delete/:_id",async(req,res)=>{
+console.log(req.params)
+ let data = await products.deleteOne(req.params)
+res.send(data)
+})
+
+app.put("/update/:_id",async(req,res)=>{
+     let data = await products.updateOne(
+        req.params,
+        {$set:req.body}
+     )
+    res.send(data)
+    })
